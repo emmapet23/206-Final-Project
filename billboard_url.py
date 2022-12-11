3e0e59f0ac58ee972c8c3a2bf284f228cf438951
emily veguilla <emilyveguilla@Emilys-MacBook-Pro-86.local>
2022-12-11 15:09:48 -0500

    idk

--- billboard_url.py
+++ billboard_url.py
@@ -79,7 +79,7 @@ def open_database(db_name):
 
 # making the table
 def make_billboard_table(tuples, cur, conn):
-    cur.execute("CREATE TABLE IF NOT EXISTS Billboard_Data (song_id PRIMARY KEY, song_title TEXT UNIQUE, song_rank INTEGER, artist_id INTEGER)")
+    cur.execute("CREATE TABLE IF NOT EXISTS Billboard_Data (song_id PRIMARY KEY, song_title TEXT UNIQUE, song_rank INTEGER)")
     id_num = 0
     num = cur.execute("SELECT max(song_id) FROM Billboard_Data").fetchone()[0]
     # print(num)
@@ -90,10 +90,18 @@ def make_billboard_table(tuples, cur, conn):
         id_num = i
         song = tuples[i][0]
         rank = tuples[i][1]
-        artist_id = cur.execute("SELECT id FROM Artists WHERE Artists.artist = (?)", (tuples[i][2], )).fetchone()[0]
+        # artist_name = tuples[i][2]
+        # artist_id = cur.execute('SELECT id FROM Artists WHERE artist = (?)', (artist_name,)).fetchone()[0]
+        # artist_id = cur.fetchone()[0]
+        # print(type(artist_id))
+
+        # cur.execute('SELECT id FROM Types WHERE type = ?',(types[0],)) 
+        # type_id = int(cur.fetchone()[0])
+
+
         #^^^ SHOULD THIS BE artist_id = cur.execute("SELECT id FROM Artists WHERE Artists.artist = (?)", (tuples[i][2], )).fetchone()[0]
         
-        cur.execute("INSERT OR IGNORE INTO Billboard_Data (song_id,song_title,song_rank, artist_id) VALUES (?,?,?,?)",(id_num,song,rank,artist_id))
+        cur.execute("INSERT OR IGNORE INTO Billboard_Data (song_id, song_title, song_rank) VALUES (?,?,?)",(id_num, song, rank))
     conn.commit()
 
 
