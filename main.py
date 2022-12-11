0b78f46a5dab6305c3421b0c43618f7bcfb16b34
Emma Peterson <emmapeterson@MacBook-Pro-785.local>
2022-12-11 16:14:08 -0500

    3 vis works

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import csv
 
 import plotly.graph_objects as go
+import plotly.express as px
 
 import spotify_api
 import last_fm_api
@@ -53,7 +54,6 @@ def pie_chart(percent):
 def vis2_get_longest_songs(database_name, cur, conn):
     conn = sqlite3.connect(database_name)
     cur = conn.cursor()
-    # sorted_lengths = sorted(cur.execute("SELECT duration FROM Durations").fetchall(), reverse=True)
 
     sorted_lengths = sorted(cur.execute("SELECT Billboard_Data.song_title, Durations.duration FROM Billboard_Data \
         JOIN Durations ON Billboard_Data.song_id = Durations.id").fetchall(), key = lambda t: t[1], reverse = True)
@@ -79,10 +79,44 @@ def vis2_get_longest_songs(database_name, cur, conn):
     return combined_lst
     
 
-def bar_graph(combined_lst):
+def long_bar_graph(combined_lst):
     fig = go.Figure(
         data = [go.Bar(x = combined_lst[0][0:10], y = combined_lst[1][0:10], marker_color = 'rgb(52,91,94)')],
         layout = dict(title = dict(text = 'Top 10 Longest Songs in the Billboard Hot 100 in Minutes'))
+    )
+    # fig = px.bar(x = combined_lst[0][0:10], y = combined_lst[1][0:10], color='rgb(52,91,94)',
+    # labels=dict(x="Song Title", y="Song Length"))
+    fig.show()
+
+def vis3_get_shortest_songs(database_name, cur, conn):
+    conn = sqlite3.connect(database_name)
+    cur = conn.cursor()
+
+    sorted_lengths = sorted(cur.execute("SELECT Billboard_Data.song_title, Durations.duration FROM Billboard_Data \
+        JOIN Durations ON Billboard_Data.song_id = Durations.id").fetchall(), key = lambda t: t[1])
+
+    x_list = []
+    y_list = []
+    combined_lst = []
+
+    if len(x_list)<=10 and len(y_list)<=10:
+        for tup in sorted_lengths:
+            song_x = tup[0]
+            time_y = tup[1]
+            x_list.append(song_x)
+            y_list.append(time_y)
+    else:
+        pass
+    combined_lst.append(x_list)
+    combined_lst.append(y_list)
+
+    print(combined_lst)
+    return combined_lst
+
+def short_bar_graph(combined_lst):
+    fig = go.Figure(
+        data = [go.Bar(x = combined_lst[0][0:10], y = combined_lst[1][0:10], marker_color = 'rgb(252,148,3)')],
+        layout = dict(title = dict(text = 'Top 10 Shortest Songs in the Billboard Hot 100 in Minutes'))
     )
     fig.show()
 
@@ -95,6 +129,8 @@ def main():
     user_num_songs = spotify_lastfm_join(cur, conn)
     visualization_one = pie_chart(user_num_songs)
     top_ten_longest = vis2_get_longest_songs('MusicData.db', cur, conn)    
-    visualization_two = bar_graph(top_ten_longest)
+    visualization_two = long_bar_graph(top_ten_longest)
+    top_ten_shortest = vis3_get_shortest_songs("MusicData.db", cur, conn)
+    visualization_three = short_bar_graph(top_ten_shortest)
 
 main()
